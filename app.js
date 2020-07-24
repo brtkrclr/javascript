e7406f5334e8d68f73377559032d6afd1dab4884
brtkrclr <68691845+brtkrclr@users.noreply.github.com>
2020-07-24 12:13:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,28 @@ var shoppinglist = [
 ];
 
 console.log(car.make)
+
+array.shift(); // Delete's first element
+array.pop(); // Delete's last element
+
+// Add new values to the beginning of the array
+array.unshift("new", "values");
+
+// Add new values to the end of the array
+array.push("ending", "values");
+
+// Splice can do anything remember
+// splice( point, delete, add values... )
+array.splice(2, 2, "new elemets", [], {}, 200, 300, function() {});
+
+
+function name() {
+    var fullname = "Berat Karacalar";
+
+    function concat(name) {
+        return "MR." + name;
+    }
+
+    return concat(fullname);
+}
+
