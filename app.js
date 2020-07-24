49d896de310784f508cc92b0d30f49bfde5c26aa
brtkrclr <68691845+brtkrclr@users.noreply.github.com>
2020-07-24 16:31:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,21 +28,7 @@ var shoppinglist = [
     "juice"
 ];
 
-console.log(car.make)
-
-array.shift(); // Delete's first element
-array.pop(); // Delete's last element
-
-// Add new values to the beginning of the array
-array.unshift("new", "values");
-
-// Add new values to the end of the array
-array.push("ending", "values");
-
-// Splice can do anything remember
-// splice( point, delete, add values... )
-array.splice(2, 2, "new elemets", [], {}, 200, 300, function() {});
-
+console.log(car.make);
 
 function name() {
     var fullname = "Berat Karacalar";
@@ -53,4 +39,68 @@ function name() {
 
     return concat(fullname);
 }
+///////////////////////////////////////
+console.log(myName, printName());
 
+var myName = "Berat";
+
+function printName() {
+    return "Karacalar";
+}
+//scope
+var engine = {
+    maker: "Ford",
+    headGAsket: {
+        pots: [
+            "piston1",
+            "piston2"
+        ]
+    }
+}
+
+//constructive function
+
+function Apple(x, y, color, score) {
+    this.color = color;
+    this.score = score;
+    this.x = x;
+    this.y = y;
+}
+new Apple(12, 56, "red", 200);
+
+
+//if statements.
+var carsLeft = 1,
+    carsRight = 0,
+    greenMan = "yes";
+
+if (greenMan === "yes") {
+    console.log('cross the road');
+} else if (greenMan === "yes" && (carsLeft === 0 || carsRight === 0)) {
+    console.log('All clear! Cross the road');
+} else {
+    console.log("Stay where you are!");
+}
+
+//for loops
+
+var classRegister = ["Lawrence", "John", "Jeff"];
+for (var i = 0; i < classRegister.length; i++) {
+    console.log(classRegister[i])
+
+}
+
+//or
+
+for (var index in classRegister) {
+    console.log(classRegister[index])
+}
+
+/*
+Eckman body is the one that controls the standards for the language and they also bring out new versions of the language
+*/
+
+//ECMA6 Part
+var symbolName = "value reference in memory";
+let letSymbol = "scoped value";
+const symName = "constand value in memory"; //cannot assign a new value to constant.
